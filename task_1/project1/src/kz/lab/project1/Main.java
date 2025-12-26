7b0a92aa58a56bcec56ec897188acf8d23dddc6c
Alik0707 <a_zhanybekov@kbtu.kz>
2025-12-26 18:32:21 +0500

    task 1

--- task_1/project1/src/kz/lab/project1/Main.java
+++ task_1/project1/src/kz/lab/project1/Main.java
@@ -1,19 +1,26 @@
 import kz.lab.project1.TransactionInterface;
+import kz.lab.project1.TransactionMode;
 import kz.lab.project1.TransactionService;
 
 void main() {
     IO.println("Project1...");
 
-    // Код по умолчанию работает в режиме MODE1
-    TransactionInterface transactionInterface = new TransactionService();
 
     String cardNumber = "4111111111111111";
     long amount = 100;
-    String transactionId = transactionInterface.startTransaction(cardNumber, amount);
-
-    IO.println(String.format("TransactionId: %s", transactionId));
-
     boolean success = true;
-    String result = transactionInterface.completeTransaction(transactionId, success);
-    IO.println(String.format("Result: %s", result));
+
+
+    // mode 1
+    TransactionInterface mode1 = new TransactionService(TransactionMode.MODE1);
+    String transactionId_mode1 = mode1.startTransaction(cardNumber, amount);
+    IO.println("MODE1 TransactionId: " + transactionId_mode1);
+    IO.println("MODE1 Result: " + mode1.completeTransaction(transactionId_mode1, success));
+
+    // mode 2
+
+    TransactionInterface mode2 = new TransactionService(TransactionMode.MODE2);
+    String transactionId_mode2 = mode2.startTransaction(cardNumber, amount);
+    IO.println("MODE2 TransactionId: " + transactionId_mode2);
+    IO.println("MODE2 Result: " + mode2.completeTransaction(transactionId_mode2, success));
 }
--- task_1/project1/src/kz/lab/project1/TransactionService.java
+++ task_1/project1/src/kz/lab/project1/TransactionService.java
@@ -1,6 +1,13 @@
 package kz.lab.project1;
 
 public class TransactionService implements TransactionInterface {
+
+    private final TransactionMode mode;
+
+    public TransactionService(TransactionMode mode) {
+        this.mode = mode;
+    }
+
     @Override
     public String startTransaction(String cardNumber, long amount) {
         return ApiService.getInstance().start(cardNumber, amount);
@@ -8,6 +15,9 @@ public class TransactionService implements TransactionInterface {
 
     @Override
     public String completeTransaction(String transactionId, boolean success) {
-        return ApiService.getInstance().completeMode1(transactionId, success);
+        return switch (mode) {
+            case MODE1 -> ApiService.getInstance().completeMode1(transactionId, success);
+            case MODE2 -> ApiService.getInstance().completeMode2(transactionId, success);
+        };
     }
 }
