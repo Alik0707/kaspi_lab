4483126da76c725b0e01651393649660f27b8cb3
Alik0707 <a_zhanybekov@kbtu.kz>
2026-01-07 18:42:23 +0500

    Convert task_3 from gitlink to normal folder

---
+++ task_3/lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Order.java
@@ -0,0 +1,20 @@
+package kz.lab.module1.advanced;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public record Order(long id, double price, LocalDateTime orderDate, Set<String> items) {
+
+    public Order {
+        if (price <= 0) {
+            throw new IllegalArgumentException("Price must be > 0");
+        }
+        if (orderDate.isAfter(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Order date must be in the past");
+        }
+        // Создаём неизменяемый сет
+        items = Collections.unmodifiableSet(new HashSet<>(items));
+    }
+}
